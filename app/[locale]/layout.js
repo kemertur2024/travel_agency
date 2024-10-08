7e9b3a0fc5446304e9c8d6e08fa31531dc731481
mikle1978-mo <azimuth-m@yandex.ru>
2024-10-08 13:12:05 +0300

    241008-metrika

--- app/[locale]/layout.js
+++ app/[locale]/layout.js
@@ -34,6 +34,49 @@ export default async function RootLayout({ children, params }) {
             namespaces={i18nNameSpaces}
         >
             <html lang={locale} className={open_sans.className}>
+                <head>
+                    <meta
+                        httpEquiv='content-type'
+                        content='text/html; charset=UTF-8'
+                    />
+                    <meta name='msapplication-TileColor' content='#00aba9' />
+                    <meta name='theme-color' content='#ffffff' />
+
+                    <meta
+                        name='yandex-verification'
+                        content='4910d4d4a23be827'
+                    />
+
+                    {/*Yandex.Metrika counter*/}
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+      
+                ym(98576941, "init", {
+                      clickmap:true,
+                      trackLinks:true,
+                      accurateTrackBounce:true
+                });
+              `,
+                        }}
+                    />
+                    <noscript>
+                        <div>
+                            <img
+                                src='https://mc.yandex.ru/watch/98576941'
+                                style={{
+                                    position: "absolute",
+                                    left: "-9999px",
+                                }}
+                                alt=''
+                            />
+                        </div>
+                    </noscript>
+                    {/* /Yandex.Metrika counter */}
+                </head>
                 <body>
                     <Header />
                     <main>{children}</main>
