81cbf2bd40ba15fc3cdd2631ebdc49d33a861fb1
kemertur2024 <kemertur2024@gmail.com>
2024-10-15 13:54:55 +0300

    241015-google tag

--- app/[locale]/layout.js
+++ app/[locale]/layout.js
@@ -75,6 +75,20 @@ export default async function RootLayout({ children, params }) {
                             />
                         </div>
                     </noscript>
+                    <script
+                        async
+                        src='https://www.googletagmanager.com/gtag/js?id=AW-16741464150'
+                    ></script>
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+                            gtag('config', 'AW-16741464150');
+                        `,
+                        }}
+                    />
                     {/* /Yandex.Metrika counter */}
                 </head>
                 <body>
